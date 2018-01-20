e2555184d4c30debe145219a313360de5c436fc5
XieShuren <1003830173@qq.com>
2018-01-20 16:00:03 +0800

    init project

---
+++ src/main/java/spring/learn/Computer.java
@@ -0,0 +1,14 @@
+package spring.learn;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Computer implements Item{
+
+	public void name() {
+		// TODO Auto-generated method stub
+		System.out.println("my name is Computer");
+	}
+	
+	
+}
---
+++ src/main/java/spring/learn/Item.java
@@ -0,0 +1,5 @@
+package spring.learn;
+
+public interface Item {
+	public void name();
+}
