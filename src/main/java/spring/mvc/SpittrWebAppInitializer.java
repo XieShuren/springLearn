f444f8d7ab7de91070e64a2bdd780f17e65ecf57
XieShuren <1003830173@qq.com>
2018-02-10 21:07:55 +0800

    Update springMvc

---
+++ src/main/java/spring/mvc/SpittrWebAppInitializer.java
@@ -0,0 +1,29 @@
+package spring.mvc;
+
+import java.util.ArrayList;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+public class SpittrWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
